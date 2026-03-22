4126582fa533d69e4f51c3cd1aa3c015385d674a
Akash Yadav <akash2610yadav@gmail.com>
2026-03-22 16:12:45 +0530

    Remove favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     "A curated community for corporate professionals in Ahmedabad to rediscover their passions live music, storytelling, stand up comedy, and more.",
   keywords: ["Ahmedabad nightlife", "corporate community", "live events", "music storytelling comedy"],
   icons: {
-    icon: '/favicon.jpg',
+    icon: '/favicon.png',
   },
 };
 
